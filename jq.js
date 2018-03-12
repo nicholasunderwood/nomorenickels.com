b4b70fcd920c6ef14455532603f086fc41cbc777
NicholasNachos <nachos.underwood@gmail.com>
2018-03-12 15:15:16 -0700

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,10 +1,6 @@
-function main() {
-    $('#div').hide();
-}
 $(document).ready(function() {
     $('#div').hide();
     $('#but').on('click', function () {
-        console.log('click')
         $(this).prev().slideToggle(500).delay(100).slideToggle(500)
     })
 });
