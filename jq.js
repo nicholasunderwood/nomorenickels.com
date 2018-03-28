bc624fdf25a2dfd95bef5c56a5cce0883736b4d3
NicholasNachos <nachos.underwood@gmail.com>
2018-03-27 19:03:25 -0700

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,6 +1,25 @@
-$(document).ready(function() {
-    $('#div').hide();
-    $('#but').on('click', function () {
-        $(this).prev().slideToggle(500).delay(100).slideToggle(500)
-    })
-});
+var tod = 'AM'
+var x = 1
+getTime()
+setInterval(getTime, 1000)
+    
+function getTime(){
+    var date = new Date();
+    var hours = date.getHours()
+    var minutes = getNumber(date.getMinutes())
+    var seconds = getNumber(date.getSeconds())
+    console.log(hours, hours>12)
+    if(hours>12){
+        hours-=12
+        tod = 'PM'
+    }
+    $('#div').text(hours + ':' + minutes + ':' + seconds + ' ' + tod)
+    console.log(hours + ':' + minutes + ':' + seconds + ' ' + tod)
+}
+
+function getNumber(t){
+    if(t<10){
+        t = '0'+ t
+    }
+    return t
+}
