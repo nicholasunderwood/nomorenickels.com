7157dc0560fbebf52e900bee39f2ca01701d86f4
Nicholas Underwood <nachos.underwood@gmail.com>
2018-05-07 15:26:57 -0700

    Update minesweeper.js

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -5,24 +5,23 @@ var dead = false;
 
 
 function isWin(){
-    var bombsFlagged = true;
-    var allClicked = true;
-    for(i=0;i<bombs.length;i++){
-        if(bombs[i].getAttribute('flag') === 'false'){
-            bombsFlagged = false;
-            break;
+    for(i=0;i<board.length;i++){ 
+        for(q=0;q<board[i].length;q++){
+            console.log(board[i]);
+            var td = board[i][q];
+            if(!testForBomb(parseInt(td.getAttribute('row')), parseInt(td.getAttribute('col'))) &&
+            !td.getAttribute('tile') !== "none"){
+                console.log('false');
+                //return false;
+            }
         }
     }
-    for(i=0;i<board.length;i++){
-        if(!testForBomb(parseInt(board[i].getAttribute('row')), parseInt(board[i].getAttribute('col'))) && 
-            !board[i].style.backgroundImage === "url('png/square.png')"){
-            
-        }
-    }
+    return true;
 }
 
 function click(td){
-    if(!dead && td.getAttribute('flag') === 'false'){
+    var win = isWin();
+    if(!dead && win && td.getAttribute('tile') !== 'flag'){
         var x = parseInt(td.getAttribute('row'));
         var y = parseInt(td.getAttribute('col'));
         var around = getSorounding(td);
@@ -38,6 +37,7 @@ function click(td){
         }
         else{
             td.style.backgroundImage = '';
+            td.setAttribute('tile', 'none');
             console.log(getCount(td));
             if(getCount(td)>0){
                 td.innerText = getCount(td);
@@ -45,8 +45,8 @@ function click(td){
             else{
                 for(w=0;w<around.length;w++){
                     around[w].style.backgroundImage = '';
+                    around[w].setAttribute('tile', 'none');
                     if(getCount(around[w])>0){
-                        console.log(around[w]);
                         around[w].innerText = getCount(around[w]);
                         around[w].style.backgroundImage = '';
                     }
@@ -54,7 +54,6 @@ function click(td){
                         var around2 = getSorounding(around[w]);
                         for(f=0;f<around2.length;f++){
                             if(!isTD(around2[f], around)){
-                                console.log(around2[f]);
                                 around.push(around2[f]);
                             }
                         }
@@ -62,6 +61,12 @@ function click(td){
                 }
             }
         }
+    }
+    else if(dead){
+        console.log('You died')
+    }
+    else if(win){
+        console.log('You Win')
     }
 }
 
@@ -95,25 +100,26 @@ function getSorounding(td){
     var around = [];
     var x = parseInt(td.getAttribute('row'));
     var y = parseInt(td.getAttribute('col'));
+    console.log(x, y);
     if(x>0){
         around.push(board[x-1][y]);
         if(y>0){
             around.push(board[x-1][y-1])
         }
     }
-    if(x<17){
+    if(x<16){
         around.push(board[x+1][y]);
-        if(y<31){
+        if(y<30){
             around.push(board[x+1][y+1])
         }
     }
     if(y>0){
         around.push(board[x][y-1]);
-        if(x<17){
+        if(x<16){
             around.push(board[x+1][y-1])
         }
     }
-    if(y<31){
+    if(y<30){
         around.push(board[x][y+1]);
         if(x>0){
             around.push(board[x-1][y+1])
@@ -162,7 +168,7 @@ for(i=0;i<17;i++){
         boardRow.push(td);
         td.setAttribute('row', i);
         td.setAttribute('col', f);
-        td.setAttribute('flag', 'false');
+        td.setAttribute('tile', 'square');
         td.style.backgroundImage = "url('png/square.png')";
         td.addEventListener('click', function(e){
             console.log(e.button);
@@ -170,16 +176,15 @@ for(i=0;i<17;i++){
         });
         td.addEventListener('contextmenu', function (e) {
             e.preventDefault();
-            console.log(typeof(e.path[0].getAttribute('flag')));
-            if(e.path[0].getAttribute('flag') === 'true'){
-                console.log('if');
-                e.path[0].style.backgroundImage = "url(png/square.png)";
-                e.path[0].setAttribute('flag', 'false')
-            }
-            else{
-                console.log('else');
+            console.log('flag');
+            if(e.path[0].getAttribute('tile') === 'square'){
+                console.log(e.path[0].style.backgroundImage);
                 e.path[0].style.backgroundImage = "url('png/flag.png')";
-                e.path[0].setAttribute('flag', 'true');
+                e.path[0].setAttribute('tile', 'flag');
+            }
+            else if(e.path[0].getAttribute('tile') === 'flag'){
+                e.path[0].style.backgroundImage = "url(png/square.png)";
+                e.path[0].setAttribute('tile', 'square')
             }
         });
         tr.appendChild(td);
