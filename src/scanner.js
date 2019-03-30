9980fcee5b0b98f19e3ca74e2cfc3fd78114241b
Nicholas <nachos.underwood@gmail.com>
2019-03-29 22:26:54 -0700

    scanner asthetic

--- src/scanner.js
+++ src/scanner.js
@@ -14,26 +14,16 @@ let scanner = new Instascan.Scanner({
 });
 
 scanner.addListener('scan', function (e) {
-    let result = e.content;
-    let match = ['']
-    let x = 0;
-    console.log(result);
-    for(let i in result){
-        if(i != ','){
-            data[x] += result[i];
-        }
-        else{
-            x++;
-            data[x].push('');
-        }
-    }
+    console.log(e)
+    let match = e.split(',');
     console.log(match)
     data.push(match)
     let tr = $('<tr></tr>');
     for(let i in match){
         let td = $('<td>'+match[i]+'</td>')
+        tr.append(td);
     }
-    console.log($.csv.fromArrays(match));
+    $('#table').append(tr);
 });
 
 function toggleScanner(btn){
@@ -45,5 +35,42 @@ function toggleScanner(btn){
     else{
         scanner.start();
         $(btn).text("Stop Scanner");
+    }
+}
+function exportToCSV(filename) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    var csvFile = '';
+    for (var i = 0; i < data.length; i++) {
+        csvFile += processRow(data[i]);
+    }
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
     }
 }
