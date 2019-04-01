47d928f7c320c7224a781ff87ad4143baa49b2f8
Nicholas <nachos.underwood@gmail.com>
2019-04-01 13:34:18 -0700

    almost done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let lastIndex = 0
 $('#programs').css('left', '150%');
 $('#about').css('left', '300%');
 function render(index){
+    let margin = 33*index + '%';
+    $('#underline').animate({marginLeft: margin}, 500)
     let bodies = ['#games', '#programs', '#about']
     if(index !== lastIndex){
         if(index>lastIndex){
@@ -16,3 +18,8 @@ function render(index){
         lastIndex = index;
     }
 }
+
+$('.box').on('transitionend', (e)=>{
+    console.log(e.currentTarget)
+    $($(e.currentTarget).children()).children()[2].style.opactiy = '100%'
+})
