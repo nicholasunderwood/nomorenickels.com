52173af58a4be1c714cb241fcc939f25a28dfba6
NicholasUnderwood <nachos.underwood@gmail.com>
2020-10-19 16:24:55 -0700

    js changes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let lastPage = $('#about');
 let lastIndex = 0;
+let w = window.innerWidth
+
+var isMoving = false
 
 const numPages = 4;
 
 
 console.log($('.wrapper:not(:first-child)'));
 $('.wrapper:not(:first-child)').css('left', '120%');
+$('#underline').css('left', 10 + (w-20)/25 - (.21*w - (w-20)/5)/2 + 'px')
 
 $('.slide').click((e) => {
+    if(isMoving) return;
+    isMoving = true;
+
     console.log("click")
 
     let slide = $(e.currentTarget);
@@ -18,20 +25,29 @@ $('.slide').click((e) => {
     let currentPage = $(slide.attr('lnk'));
     console.log(slide.attr('lnk'), currentPage)
     lastPage.animate({left: index > lastIndex ? '-120%' : '120%'}, 500);
-    currentPage.animate({left: '5%'}, 500);
+    currentPage.animate({left: '5%'}, { 
+        duration: 500,
+        complete: () => { isMoving = false; }
+    });
 
-    let w = window.innerWidth
+    if(index - lastIndex > 1){
+        console.log(1, $('.wrapper').slice(lastIndex+1, index))
+        $('.wrapper').slice(lastIndex+1, index).css("left","-120%");
+    } else if (lastIndex - index > 1) {
+        console.log(1, $('.wrapper').slice(index+1, lastIndex))
+        $('.wrapper').slice(index+1, lastIndex).css("left", "120%");
+    }
 
-    boxOffset = 10 + 380*index + 76*(index+1) - (.21*w-380)/2
-    console.log(index, w, boxOffset, (w-20)*(index+1)/25)
+    w = window.innerWidth
+
+    boxOffset = 10 + (w-20)*index/5 + (w-20)*(index+1)/25 - (.21*w - (w-20)/5)/2
+    console.log(boxOffset)
     
     $('#underline').animate({left: boxOffset + "px"}, 500);
 
     lastPage = currentPage;
     lastIndex = currentPage.index();
 });
-
-$('#roboticsBtn').click()
 
 // tilting
 // const max = 40;
