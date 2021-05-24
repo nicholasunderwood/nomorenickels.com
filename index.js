ab6605793623d8840018773a490fcccdac792469
Nicholas Underwood <nachos.underwood@gmail.com>
2021-05-23 19:20:23 -0700

    gay

--- index.js
+++ index.js
@@ -1,57 +1,8 @@
-var lastPage = $('#about');
-var lastIndex = 0;
-var index = 0;
-var w = window.innerWidth
-var isMoving = false
-const numPages = 4;
-
-const animationStates = [
-    {heading: 315, pos: 80},
-    {heading: 225, pos: 80},
-    {heading: 225, pos: 10},
-    {heading: 315, pos: 10}
-];
-
-var lastState = animationStates[0];
-
-$('.wrapper:not(:first-child)').css({ left: '120%', display: 'none' });
-$('#underline').css('left', 10 + (w-20)/25 - (.21*w - (w-20)/5)/2 + 'px')
-
-$('.slide').click((e) => {
-    if(isMoving) return;
-    isMoving = true;
-
-
-    let slide = $(e.currentTarget);
-    index = $('.slide').index(slide);
-    slideToPage(index);
-    
-    
-});
-
-$('.arrow.left-arrow').click(() => {
-    if(isMoving) return;
-    slideToPage(lastIndex == 0 ? numPages - 1 : lastIndex - 1);
-});
-
-$('.arrow.right-arrow').click(() => {
-    if(isMoving) return;
-    slideToPage(lastIndex == numPages-1 ? 0 : lastIndex + 1);
-});
-
-$(window).resize(() => {
-    w = window.innerWidth;
-    console.log('resize', w)
-    $('#underline').css({left: getUnderlineOffset(index) + "px"});
-});
-
 function slideToPage(index){
-    console.log(index);
     if(index == lastIndex) return;
     
     let currentPage = $($(`.wrapper`)[index]);
     currentPage.css('display', 'flex');
-    console.log(currentPage, lastPage);
 
     lastPage.animate({left: index > lastIndex ? '-120%' : '120%'}, 500);
     currentPage.animate({left: '10%'}, { 
@@ -92,42 +43,108 @@ function getUnderlineOffset(index) {
 
 function animateBackground(index) {
     let nextState = animationStates[index];
-    console.log(nextState.heading - lastState.heading)
     let degPerStep = (nextState.heading - lastState.heading)/50;
     let percentPerStep = (nextState.pos - lastState.pos)/50;
 
-    console.log(nextState, degPerStep, percentPerStep)
 
     let step = 0;
+    let lastPage = lastIndex
 
     let inter = setInterval(() => {
         if(step >= 50){ 
             clearInterval(inter);
-            $('#container').css(getLinearGradientStyle(nextState.pos, nextState.heading));
+            let transState = Math.round(step/50*gradLength);
+            console.log(transState)
+            // $('#container').css(getLinearGradientStyle(nextState.pos, nextState.heading, lastPage, index, gradLength));
             lastState = nextState
             return;
         }
         
-        step += 1;
+        step += 2;
+        let transState = Math.round(step/50*gradLength);
         let heading = Math.floor(lastState.heading + step*degPerStep);
         let pos =  Math.floor(lastState.pos + step*percentPerStep);
-        console.log(heading, pos)
-        $('#container').css(getLinearGradientStyle(pos, heading));
+        $('#container').css(getLinearGradientStyle(pos, heading, lastPage, index, transState));
     }, 10)
 
 
 }
 
-function getLinearGradientStyle(pos, heading){
+function getLinearGradientStyle(pos, heading, lastPage, nextPage, transState){
     return {
         background: `linear-gradient(
             ${heading}deg
             ,var(--bg-color-1)
             ,var(--bg-color-1) ${pos}%
-            ,#ff8a00 ${pos}%
-            ,#e52e71 ${pos+10}%
-            ,var(--bg-color-1) ${pos+10}%
+            ${getFlagGradient(pos, prideOrder[lastPage], gradLength-transState)}
+            ${getFlagGradient(pos+gradLength-transState, prideOrder[nextPage], transState)}
+            ,var(--bg-color-1) ${pos+gradLength}%
             ,var(--bg-color-1)
         )`
     }
 }
+
+function getFlagGradient(start, flag, flagWidth){
+    let colors = pride[flag];
+    let p = flagWidth/colors.length;
+    return colors.map((hex,i) => `,${hex} ${start+p*i}% ,${hex} ${Math.round(start+p*(i+1))}% `).reduce((a,b) => a+b, '')
+}
+
+var lastPage = $('#about');
+var lastIndex = 0;
+var index = 0;
+var w = window.innerWidth
+var isMoving = false
+const numPages = 4;
+const gradLength = 15;
+
+const prideOrder = ['lgbt', 'bi', 'ace', 'trans']
+
+const animationStates = [
+    {heading: 315, pos: 70},
+    {heading: 225, pos: 70},
+    {heading: 225, pos: 20},
+    {heading: 315, pos: 20}
+];
+
+const pride = {
+    'bi': ['#D00070', '#8C4799', '#0032A0'],
+    'lgbt': ['#FF0018', '#FFA52C', '#FFFF41', '#008018', '#0000F9', '#86007D'],
+    'ace': ['#000000', '#A4A4A4', '#FFFFFF', '#810081'],
+    'trans': ['#55CDFC', '#F7A8B8', '#FFFFFF', '#F7A8B8', '#55CDFC']
+}
+
+var lastState = animationStates[0];
+
+$('.wrapper:not(:first-child)').css({ left: '120%', display: 'none' });
+$('#underline').css('left', 10 + (w-20)/25 - (.21*w - (w-20)/5)/2 + 'px')
+
+$('.slide').click((e) => {
+    if(isMoving) return;
+    isMoving = true;
+
+
+    let slide = $(e.currentTarget);
+    index = $('.slide').index(slide);
+    slideToPage(index);
+    
+    
+});
+
+$('.arrow.left-arrow').click(() => {
+    if(isMoving) return;
+    slideToPage(lastIndex == 0 ? numPages - 1 : lastIndex - 1);
+});
+
+$('.arrow.right-arrow').click(() => {
+    if(isMoving) return;
+    slideToPage(lastIndex == numPages-1 ? 0 : lastIndex + 1);
+});
+
+$(window).resize(() => {
+    w = window.innerWidth;
+    console.log('resize', w)
+    $('#underline').css({left: getUnderlineOffset(index) + "px"});
+});
+
+$('#container').css(getLinearGradientStyle(lastState.pos, lastState.heading, lastIndex, index, 15));
